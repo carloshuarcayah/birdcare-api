0503d685bda47f33b19d8ebdd476b4661517ffff
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-02-02 18:04:31 -0500

    Primer commit: Inicializacino del proyecto

---
+++ src/main/java/pe/com/birdcare/BirdCareStoreApiApplication.java
@@ -0,0 +1,13 @@
+package pe.com.birdcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdCareStoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirdCareStoreApiApplication.class, args);
+    }
+
+}
