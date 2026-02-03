82ff13585bd9fbda5d0469df91ec0adab0ac1235
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-02-03 00:29:53 -0500

    add UserController and GET endpoint for all users

---
+++ src/main/java/pe/com/birdcare/controller/UserController.java
@@ -0,0 +1,25 @@
+package pe.com.birdcare.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.com.birdcare.dto.UserResponseDTO;
+import pe.com.birdcare.service.IUserService;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final IUserService userService;
+
+    @GetMapping
+    public ResponseEntity<Page<UserResponseDTO>> getAll(
+            @PageableDefault(size = 10) Pageable pageable) {
+        return ResponseEntity.ok(userService.findAll(pageable));
+    }
+}
