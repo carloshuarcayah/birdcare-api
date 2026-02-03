59a7a1dda9800fc4a2295de8d5e93d16e9dae38e
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-02-02 22:04:42 -0500

    Add User entity

---
+++ src/main/java/pe/com/birdcare/entity/User.java
@@ -0,0 +1,54 @@
+package pe.com.birdcare.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pe.com.birdcare.enums.Role;
+
+import java.io.Serializable;
+
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100, unique = true)
+    @NotBlank(message = "email is required")
+    @Email(message = "email must be valid")
+    private String email;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+
+    @Column(nullable = false,length = 100)
+    @NotBlank(message = "name is required")
+    private String name;
+
+    @Column(name = "last_name",nullable = false,length = 100)
+    @NotBlank(message = "lastname is required")
+    private String lastName;
+
+    @Column(nullable = true,length = 15)
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20,nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    @NotNull
+    private Boolean active = true;
+
+}
