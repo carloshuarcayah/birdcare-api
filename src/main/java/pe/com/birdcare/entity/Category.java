f21db6c6cc95913f5f500723709cf111cc0b1462
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-02-03 12:36:16 -0500

    fix(entity): add AllArgs and NoArgs annotation in Category repository

--- src/main/java/pe/com/birdcare/entity/Category.java
+++ src/main/java/pe/com/birdcare/entity/Category.java
@@ -4,15 +4,14 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.validator.constraints.UniqueElements;
 
 @Entity
 @Table(name = "categories")
 @Getter @Setter
 @Builder
+@AllArgsConstructor @NoArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
