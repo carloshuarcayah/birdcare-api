6c8f05b364df66eb36c8f81be612c3dfeaf7f3b4
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-02-06 11:58:26 -0500

    feat(dto): create Request DTO for Order and OrderItem

---
+++ src/main/java/pe/com/birdcare/dto/OrderRequestDTO.java
@@ -0,0 +1,19 @@
+package pe.com.birdcare.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+public record OrderRequestDTO(
+        @NotNull(message = "User ID is required")
+        Long userId,
+
+        @NotBlank(message = "Shipping address is required")
+        String shippingAddress,
+
+        @NotEmpty(message = "The order must have at least one item")
+        List<OrderItemRequestDTO> items
+) {
+}
