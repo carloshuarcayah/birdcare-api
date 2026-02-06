3752f4cf12871ce24c76960a82297c181e3528da
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-02-06 00:48:47 -0500

    feat(entity): create Product entity with Category relation

---
+++ src/main/java/pe/com/birdcare/entity/Product.java
@@ -0,0 +1,42 @@
+package pe.com.birdcare.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@AllArgsConstructor @NoArgsConstructor
+@Getter @Setter
+@Builder
+public class Product implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @NotNull
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @NotBlank
+    @Column(length = 100,nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "TEXT",nullable = true)
+    @Builder.Default
+    private String description = "No description yet for this product.";
+
+    @NotNull
+    @PositiveOrZero
+    @Column(nullable = false, precision = 10,scale = 2)
+    private BigDecimal price;
+
+    @NotNull
+    @Column(nullable = false)
+    @PositiveOrZero
+    private Integer stock;
+}
