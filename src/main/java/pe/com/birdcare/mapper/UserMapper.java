dbfee3ceddeb5add298130c7ef4d6cee5bc8a3a9
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-02-11 00:05:50 -0500

    refactor(mapper): improve UserMapper with new DTO structure

--- src/main/java/pe/com/birdcare/mapper/UserMapper.java
+++ src/main/java/pe/com/birdcare/mapper/UserMapper.java
@@ -2,18 +2,20 @@ package pe.com.birdcare.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.Mappings;
-import pe.com.birdcare.dto.UserRequestDTO;
+import pe.com.birdcare.dto.UserCreateDTO;
 import pe.com.birdcare.dto.UserResponseDTO;
+import pe.com.birdcare.dto.UserUpdateDTO;
 import pe.com.birdcare.entity.User;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     @Mapping(target = "password", ignore = true)
-    void updateEntityFromDto(UserRequestDTO dto, @MappingTarget User user);
+    void updateUser(UserUpdateDTO dto, @MappingTarget User user);
 
     @Mapping(target = "password", ignore = true)
-    User fromRequestToEntity(UserRequestDTO req);
+    User createUser(UserCreateDTO req);
 
-    UserResponseDTO toResponseDTO(User user);
+    UserResponseDTO response(User user);
+
+
 }
