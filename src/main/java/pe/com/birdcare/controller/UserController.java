08d547a8131a94f40aff195d896f6b41bb2bc134
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-02-11 16:21:17 -0500

    feat(controller): add user endpoints (/me)

--- src/main/java/pe/com/birdcare/controller/UserController.java
+++ src/main/java/pe/com/birdcare/controller/UserController.java
@@ -2,11 +2,10 @@ package pe.com.birdcare.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import pe.com.birdcare.dto.UserCreateDTO;
 import pe.com.birdcare.dto.UserPasswordChangeDTO;
@@ -20,28 +19,10 @@ import pe.com.birdcare.service.IUserService;
 public class UserController {
     private final IUserService userService;
 
-    @GetMapping
-    public ResponseEntity<Page<UserResponseDTO>> getAll(
-            @PageableDefault(size = 10) Pageable pageable) {
-        return ResponseEntity.ok(userService.findAll(pageable));
-    }
-
-    @GetMapping("/active")
-    public ResponseEntity<Page<UserResponseDTO>> getJustActives(
-            @PageableDefault(size = 10) Pageable pageable) {
-        return ResponseEntity.ok(userService.findJustActives(pageable));
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<UserResponseDTO> getById(@PathVariable Long id) {
-        return ResponseEntity.ok(userService.findById(id));
-    }
-
-    @GetMapping("/search")
-    public ResponseEntity<Page<UserResponseDTO>> getByName(
-            @RequestParam String name,
-            @PageableDefault(size = 10) Pageable pageable) {
-        return ResponseEntity.ok(userService.findByName(pageable, name));
+    //CUSTOMER - ADMIN
+    @GetMapping("/me")
+    public ResponseEntity<UserResponseDTO> getUser(@AuthenticationPrincipal UserDetails auth){
+        return ResponseEntity.ok(userService.findMe(auth.getUsername()));
     }
 
     @PostMapping
@@ -49,29 +30,19 @@ public class UserController {
         return new ResponseEntity<>(userService.create(request), HttpStatus.CREATED);
     }
 
-    @PutMapping("/{id}")
+    //USER
+    @PutMapping("/me")
     public ResponseEntity<UserResponseDTO> update(
-            @PathVariable Long id,
+            @AuthenticationPrincipal UserDetails user,
             @Valid @RequestBody UserUpdateDTO request) {
-        return ResponseEntity.ok(userService.update(request, id));
+        return ResponseEntity.ok(userService.update(request, user.getUsername()));
     }
 
-    @DeleteMapping("/{id}")
+    //CUSTOMER
+    @PatchMapping("/me/password")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id) {
-        userService.delete(id);
+    public void changePassword(@AuthenticationPrincipal UserDetails auth, @Valid @RequestBody UserPasswordChangeDTO req) {
+        userService.changePassword(auth.getUsername(), req);
     }
 
-    @PutMapping("/{id}/enable")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void enable(
-            @PathVariable Long id) {
-        userService.enable(id);
-    }
-
-    @PatchMapping("/{id}/password")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void changePassword(@PathVariable Long id, @Valid @RequestBody UserPasswordChangeDTO req) {
-        userService.changePassword(id, req);
-    }
 }
