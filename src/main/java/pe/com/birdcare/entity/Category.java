788e974aa15a83cf513175b1eb3a0e1ebdd30398
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-02-11 17:35:04 -0500

    chore(entity): remove unused imports

--- src/main/java/pe/com/birdcare/entity/Category.java
+++ src/main/java/pe/com/birdcare/entity/Category.java
@@ -3,9 +3,7 @@ package pe.com.birdcare.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.*;
-import org.hibernate.validator.constraints.UniqueElements;
 
 @Entity
 @Table(name = "categories")
