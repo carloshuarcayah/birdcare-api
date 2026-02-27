16d7c488af1d89559100ebacc86cf2eed4dda213
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-02-27 01:54:29 -0500

    refactor(order-orderitem): refactorizacion completa del flujo de una orden

--- src/main/java/pe/com/birdcare/mapper/OrderItemMapper.java
+++ src/main/java/pe/com/birdcare/mapper/OrderItemMapper.java
@@ -1,23 +1,15 @@
 package pe.com.birdcare.mapper;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-import pe.com.birdcare.dto.OrderItemRequestDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import pe.com.birdcare.dto.OrderItemResponseDTO;
 import pe.com.birdcare.entity.OrderItem;
 
-import java.math.BigDecimal;
+@Mapper(componentModel = "spring")
+public interface OrderItemMapper {
 
-@Component
-public class OrderItemMapper {
-    public OrderItemResponseDTO toResponse(OrderItem req){
-        BigDecimal subtotal = req.getPrice().multiply(BigDecimal.valueOf(req.getQuantity()));
-        return OrderItemResponseDTO.builder()
-                .productId(req.getProduct().getId())
-                .productName(req.getProduct().getName())
-                .price(req.getPrice())
-                .quantity(req.getQuantity())
-                .subtotal(subtotal)
-                .build();
-    }
+    @Mapping(source = "product.id", target = "productId")
+    @Mapping(source = "product.name", target = "productName")
+    @Mapping(expression = "java(orderItem.getSubtotal())", target = "subtotal")
+    OrderItemResponseDTO toResponse(OrderItem orderItem);
 }
